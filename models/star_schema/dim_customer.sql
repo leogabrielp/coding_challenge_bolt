84b41f2fa14058200071fb4491b215ae6de53a50
leogabrielp <leo.gabrielp@gmail.com>
2024-12-04 02:19:02 +0100

    fix

--- models/star_schema/dim_customer.sql
+++ models/star_schema/dim_customer.sql
@@ -5,19 +5,26 @@
     tags=['star_schema']
 ) }}
 
-WITH customer AS (
+WITH 
+
+customer AS (
 
     SELECT 
-    cust.customer_id            AS customer_id,
-    cust.customer_full_name     AS customer_full_name,
-    cust.customer_group_id      AS customer_group_id,
-    cust.email                  AS email,
-    cust.phone_number           AS phone_number,
-    grou.customer_group_name    AS customer_group_name,
-    cust.meta_processing_time   AS customer_meta_processing_time
-    FROM {{ ref('staging_customer') }} cust
-    LEFT JOIN {{ ref('staging_customer_group') }} grou
-    ON cust.customer_group_id=grou.customer_group_id
+    cust.customer_id                                                            AS customer_id,
+    cust.customer_full_name                                                     AS customer_full_name,
+    cust.customer_group_id                                                      AS customer_group_id,
+    cust.email                                                                  AS email,
+    cust.phone_number                                                           AS phone_number,
+    cust.meta_processing_time                                                   AS customer_meta_processing_time,
+    SUM(CASE WHEN status IN ("Booked","Finished") THEN ord.price_eur END)       AS revenue_generated,
+    COUNT(CASE WHEN status IN ("Booked","Finished") THEN ord.order_id END)      AS orders_placed,
+    MAX(DATETIME(ord.meta_processing_time))                                     AS last_order_datetime, 
+    --assuming that order datetime is the same as the moment when the entry was ingested in staging_order table
+
+    FROM {{ ref('staging_customer') }} AS cust
+    LEFT JOIN {{ ref('staging_order') }} AS ord
+    ON cust.customer_id=ord.customer_id
+    GROUP BY 1,2,3,4,5,6
 )
 
 SELECT
@@ -26,6 +33,8 @@ customer_full_name,
 customer_group_id,
 email,
 phone_number,
-customer_group_name,
+revenue_generated,
+orders_placed,
+last_order_datetime,
 customer_meta_processing_time
 FROM customer
